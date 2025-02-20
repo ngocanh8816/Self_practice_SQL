16e3686aa669308c2aeb8998183333962c2ff91a
Ngọc Anh <ngocanhnguyenn1018@gmail.com>
2025-02-20 17:12:53 +0700

    Update and rename 19.02.2024.sql to 20.02.2024.sql

--- 20.02.2024.sql
+++ 20.02.2024.sql
@@ -9,3 +9,10 @@ from queries
 group by query_name
 order by query_name
 
+--1934. Confirmation Rate: https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
+select
+    A.user_id,
+    coalesce(round(avg((action = 'confirmed')::int),2),0) confirmation_rate
+from signups A
+left join confirmations B on A.user_id = B.user_id
+group by A.user_id
