3bd3bdd44c46342d39955583a25b1776c6e8da45
Ngọc Anh <ngocanhnguyenn1018@gmail.com>
2025-02-21 23:15:56 +0700

    Update 20.02.2024.sql

--- 20.02.2024.sql
+++ 20.02.2024.sql
@@ -16,3 +16,57 @@ select
 from signups A
 left join confirmations B on A.user_id = B.user_id
 group by A.user_id
+
+--1251. Average Selling Price: https://leetcode.com/problems/average-selling-price/description/?envType=study-plan-v2&envId=top-sql-50
+select
+    A.product_id,
+    coalesce(round(sum(price * units)/sum(units)::decimal,2),0) as average_price
+from prices A
+left join unitssold B on A.product_id = B.product_id
+where purchase_date between start_date and end_date
+      or purchase_date is null
+group by A.product_id
+
+--1633. Percentage of Users Attended a Contest: https://leetcode.com/problems/percentage-of-users-attended-a-contest/description/?envType=study-plan-v2&envId=top-sql-50
+select
+    contest_id,
+    round(count(*)/
+    (select count(*) from users)::decimal*100,2) as percentage
+from register
+group by contest_id
+order by percentage desc, contest_id
+
+--619. Biggest Single Number: https://leetcode.com/problems/biggest-single-number/description/?envType=study-plan-v2&envId=top-sql-50
+select
+case
+    when count(*) = 1 then num
+    else null
+end num
+from mynumbers
+group by num
+order by num desc nulls last
+limit 1
+--NULLs First: Giá trị NULL sẽ xuất hiện đầu tiên khi sắp xếp theo thứ tự tăng dần (ASC).
+--NULLs Last: Giá trị NULL sẽ xuất hiện cuối cùng khi sắp xếp theo thứ tự giảm dần (DESC).
+--Cách khác: tìm dãy số chứa các số chỉ xuất hiện đúng 1 lần. Sau đó tìm giá trị lớn nhất trong dãy số đó
+select
+    max(num) as num
+from
+(
+    select num
+    from mynumbers
+    group by num
+    having count(*) = 1
+) cte
+
+--180. Consecutive Numbers: https://leetcode.com/problems/consecutive-numbers/description/?envType=study-plan-v2&envId=top-sql-50
+select distinct
+    num as ConsecutiveNums 
+from
+    (
+    select*,
+    lead(num,1) over (order by id) after_num_1,
+    lead(num,2) over (order by id) after_num_2
+    from logs
+    ) cte
+where num = after_num_1 and num = after_num_2
